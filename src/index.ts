368bd025eaa4931b3284ad872de18ca8925ffcc8
silent_bayline <silent.bayline@gmail.com>
2024-11-27 00:02:37 +0900

    fix: cors error for x-saasus-referer

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: 'http://localhost:3000',
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with', 'x-saasus-referer'],
     methods: ['*'],
   })
 )
